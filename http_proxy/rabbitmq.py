77c9f0d3f42f32172ad3e4dc6b1ff42ecafc1279
Pedro Worcel <pedro@worcel.com>
2021-06-14 14:51:50 -0700

    Improve exception messaging

--- http_proxy/rabbitmq.py
+++ http_proxy/rabbitmq.py
@@ -1,11 +1,21 @@
 import pika
 import os
 
-def new_connection() -> pika.BlockingConnection:
-    credentials = pika.PlainCredentials(os.getenv("RABBIT_USERNAME"),
-        os.getenv("RABBIT_PASSWORD"))
+class MissingEnvironmentVariablesException(Exception):
+    pass
 
-    connection_parameters = pika.ConnectionParameters(os.getenv("RABBIT_HOSTNAME"), 5672, '/',
+def new_connection() -> pika.BlockingConnection:
+    hostname = os.getenv("RABBIT_HOSTNAME")
+    username = os.getenv("RABBIT_USERNAME")
+    password = os.getenv("RABBIT_PASSWORD")
+    
+    if hostname is None or username is None or password is None:
+        msg = "Could not connect to rabbit because the required environment variables are missing"
+        raise MissingEnvironmentVariablesException(msg)
+
+    credentials = pika.PlainCredentials(username, password)
+
+    connection_parameters = pika.ConnectionParameters(hostname, 5672, '/',
         credentials=credentials)
     connection = pika.BlockingConnection(connection_parameters)
 
