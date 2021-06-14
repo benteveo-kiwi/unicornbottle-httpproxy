f7a855c4d53e38a537c92b8e5360aef221c18536
Pedro Worcel <pedro@worcel.com>
2021-06-14 14:50:31 -0700

    Get configuration from environment variable.

--- http_proxy/rabbitmq.py
+++ http_proxy/rabbitmq.py
@@ -1,8 +1,12 @@
 import pika
+import os
 
 def new_connection() -> pika.BlockingConnection:
-    credentials = pika.PlainCredentials('httpproxy', 'SHJfakkjawkjhfkawjaw')
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters('localhost', 5672, '/', credentials=credentials))
+    credentials = pika.PlainCredentials(os.getenv("RABBIT_USERNAME"),
+        os.getenv("RABBIT_PASSWORD"))
+
+    connection_parameters = pika.ConnectionParameters(os.getenv("RABBIT_HOSTNAME"), 5672, '/',
+        credentials=credentials)
+    connection = pika.BlockingConnection(connection_parameters)
 
     return connection
